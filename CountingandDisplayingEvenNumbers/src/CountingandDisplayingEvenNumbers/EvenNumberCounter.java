af12df9a5630005fccf663ef7a9957ae373c815f
Blessings Wandji <120319884+Bledsings@users.noreply.github.com>
2023-09-26 17:47:34 +0100

    completed exercise

---
+++ CountingandDisplayingEvenNumbers/src/CountingandDisplayingEvenNumbers/EvenNumberCounter.java
@@ -0,0 +1,13 @@
+package CountingandDisplayingEvenNumbers;
+
+
+public class EvenNumberCounter {
+	public static void main(String[] args) {
+		for(int i=2;i<=10;i+=2) {
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
